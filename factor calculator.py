36aee9bc6b2bdf367d22d26630c888d471f36c1f
Samuel-Paladino <118221675+kidkicksam76@users.noreply.github.com>
2025-04-28 10:25:02 -0700

    yo

---
+++ factor calculator.py
@@ -0,0 +1,13 @@
+def find_factors(number):
+    factors = []
+    for possible_factor in range(1, abs(number) + 1):
+        if number % possible_factor == 0:
+            factors.append(possible_factor)
+            factors.append(-possible_factor)
+    return sorted(set(factors))
+
+number = int(input("Enter a number: "))
+
+factors = find_factors(number)
+
+print(f"The factors of {number} are: {factors}")
